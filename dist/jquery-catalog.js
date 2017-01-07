2db9c3d8eb01f1284b9a50794cf708988bf589de
InsertSweat <769356353@qq.com>
2017-01-07 11:46:59 +0800

    add toggleActive

--- dist/jquery-catalog.js
+++ dist/jquery-catalog.js
@@ -1,62 +1,101 @@
-$.fn.catalog = function(options) {
-	var defaults = {
-			'box': '.catalog'
-		},
-		opt = $.extend({}, defaults, options),
-		header = this.children(':header');
 
-	var addIndex = function() { 
-		header.map(function(i,e){
-			var index = 'catalog' + '-' + e.tagName + '-' + i; 
-			e.id = index;  
-		}) 
+(function($){
+	$.fn.catalog = function(options) {
+		var opt = $.extend({
+				'box': '.catalog'
+			}, options),
+			header = this.children(':header');
 
-	}
+		return this.each(function() {
 
-	var addLink = function() {
-		var data = [],
-		prev = header[0].tagName.charAt(1),
-		lv = 0, //深度
-		li = null;
+			//给标题添加ID
+			var addIndex = function() { 
+				header.map(function(i,e){
+					var index = 'catalog' + '-' + e.tagName + '-' + i; 
+					e.id = index;  
+				}) 
 
-		//初始化根目录
-		data[0] = $('<ul></ul>');
-		data[0].appendTo($(opt.box));
-		
-		header.map(function(i,e){
-			li = $('<li></li>');
-			li.append('<a href="#' + e.id + '">' + e.innerHTML +'</a>'); 
-
-			// 根据标签等级增加或减小深度
-			if(prev > e.tagName.charAt(1)){
-				lv -= parseInt(prev) - parseInt(e.tagName.charAt(1));
-			}else if(prev < e.tagName.charAt(1)){
-				lv += parseInt(e.tagName.charAt(1)) - parseInt(prev);
-				data[lv] = $('<ul></ul>');
-				prevLi.append(data[lv]);
 			}
 
+			//生成菜单
+			var createCatalog = function() {
+				var data = [],
+				prev = header[0].tagName.charAt(1),
+				lv = 0, //深度
+				li = null;
 
-			prev = e.tagName.charAt(1) 
-			data[lv].append(li);
-			prevLi = li;
+				//初始化根目录
+				data[0] = $('<ul></ul>');
+				data[0].appendTo($(opt.box));
+				
+				header.map(function(i,e){
+					li = $('<li></li>');
+					li.append('<a href="#' + e.id + '">' + e.innerHTML +'</a>'); 
+
+					// 根据标签等级增加或减小深度
+					if(prev > e.tagName.charAt(1)){
+						lv -= parseInt(prev) - parseInt(e.tagName.charAt(1));
+					}else if(prev < e.tagName.charAt(1)){
+						lv += parseInt(e.tagName.charAt(1)) - parseInt(prev);
+						data[lv] = $('<ul></ul>');
+						prevLi.append(data[lv]);
+					}
+
+
+					prev = e.tagName.charAt(1) 
+					data[lv].append(li);
+					prevLi = li;
+				})
+			}
+
+			//切换动画
+			var bindAnimate = function() { 
+				$(opt.box + ' a').click(function(){
+
+					$(window).unbind('scroll');
+
+					var it = this;
+					$('body').animate({
+						scrollTop: $($(it).attr('href')).offset().top
+					}, 'fast',function(){
+						bindScroll();
+					})
+					toggleActive($(it));
+					return false;
+				})
+			}
+
+			var bindScroll = function() {
+				$(window).scroll(function(){
+					header.map(function(i,e){
+						if ($(window).scrollTop() > e.offsetTop) {
+							toggleActive($(opt.box + ' a[href="#' + e.id + '"]'))
+						}
+					})
+				})
+			}
+
+			var toggleActive = function(e) {
+				$(opt.box + ' a').removeClass('active');
+				e.addClass('active');
+				$(opt.box + ' ul > li > ul').hide();
+				$(opt.box + ' .active').parents('ul').show();
+				$(opt.box + ' .active ~ ul').show();
+			}
+
+			var init = function() { 
+				$(opt.box + ' ul > li > ul').hide();
+
+				addIndex();
+				createCatalog();
+				bindAnimate();
+				bindScroll();
+			}	
+			
+			init();
 		})
+
 	}
-
-	var bindAnimate = function() {
-		$(opt.box + ' a').click(function(){
-			var it = this;
-			$('body,html').animate({
-				scrollTop: $(it.getAttribute('href')).offset().top
-			}, 'fast')
-			return false
-		})
-	}
-
-	
-	addIndex();
-	addLink();
-	bindAnimate();
-}
-
+})(window.jQuery); 
 $('.container').catalog();
+
